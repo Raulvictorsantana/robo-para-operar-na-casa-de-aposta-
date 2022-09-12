1d86b796534a6ac1f3666bc4053eca69995b4035
raul victor <raulvictormello@hotmail.com>
2022-09-12 19:15:24 -0300

    add comentarios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-
+//  div 'btn' foi pego direto do browser para funcionar em outros sites precisa alterar a div para o botão que aposta e para a aposta  
 document.querySelectorAll('.btn btn-success bet font-family-title ng-star-inserted').forEach((item, index) => {
   setTimeout(() => {
 		if(!item.classList.contains('.btn btn-success bet font-family-title ng-star-inserted')){
@@ -9,6 +9,8 @@ document.querySelectorAll('.btn btn-success bet font-family-title ng-star-insert
 			
 		} else { 
 		return false;
-    }
+    }// A cada 20 segundo o programa aperta para aposta e parar a aposta apos mais 20 segundos
   }, index * 20000);
 });
+
+
